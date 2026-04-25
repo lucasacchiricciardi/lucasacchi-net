e3473544750de14186fa6104643a4fd617a3e533
Luca Sacchi Ricciardi <luca.sacchi@gmail.com>
2026-04-25 20:36:01 +0200

    fix(ci): correct Lighthouse URL

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,7 +1,7 @@
 module.exports = {
   ci: {
     collect: {
-      url: ['https://lucasacchiricciardi.github.io/the-linux-formula/'],
+      url: ['https://lucasacchiricciardi.github.io/lucasacchi-net/'],
       numberOfRuns: 3,
       settings: {
         preset: 'desktop',
